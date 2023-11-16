457a406e3c366994d38d2e4f6c9d524ce7887320
SimoneAncona <simone.ancona00@gmail.com>
2023-11-16 13:44:19 +0100

    Updated project structure

--- cardreader/build.ps1
+++ cardreader/build.ps1
@@ -1,9 +1,34 @@
+if ($args[0] -eq "--help") {
+    Write-Output "Usage:
+    build
+Options:
+    --debug     Build for debug (default)
+    --help      Show this help message
+    --release   Build for release
+    -t=linux    Build for linux
+    -t=win32    Build for Windows
+"
+    exit
+}
 if (-not (Test-Path build/)) {
     mkdir build
 }
-Set-Location build
-cmake ../
+if ($args.Contains("--release")) {
+    if ($args.Contains("-t=linux")) {
+        & wsl -e sh ./build.sh
+        exit
+    }
+    Set-Location build
+    cmake ../ --config Release
+} else {
+    if ($args.Contains("-t=linux")) {
+        & wsl -e sh ./build.sh
+        exit
+    }
+    Set-Location build
+    cmake ../
+}
 $vs = (Get-CimInstance MSFT_VSInstance -Namespace root/cimv2/vs).InstallLocation
 $msbuild = "MSBuild\Current\Bin\MSBuild.exe"
-& "$vs\$msbuild" cardreader.sln
+& "$vs\$msbuild" ((Get-Item *.sln).name)
 Set-Location ../

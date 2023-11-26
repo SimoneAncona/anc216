ce4dca9994cae3ae320aaefd2e1c3d873f863f35
SimoneAncona <simone.ancona00@gmail.com>
2023-11-27 00:30:36 +0100

    Init emulator

---
+++ emulator/build.ps1
@@ -0,0 +1,33 @@
+if ($args[0] -eq "--help") {
+    Write-Output "Usage:
+    build
+Options:
+    --debug     Build for debug (default)
+    --help      Show this help message
+    --release   Build for release
+    -t=linux    Build for linux
+    -t=win32    Build for Windows
+"
+    exit
+}
+if (-not (Test-Path build/)) {
+    mkdir build
+}
+if ($args.Contains("--release")) {
+    if ($args.Contains("-t=linux")) {
+        & wsl -e sh ./build.sh
+        exit
+    }
+    Set-Location build
+    cmake ../
+    cmake --build . --config Release
+} else {
+    if ($args.Contains("-t=linux")) {
+        & wsl -e sh ./build.sh
+        exit
+    }
+    Set-Location build
+    cmake ../
+    cmake --build .
+}
+Set-Location ../
